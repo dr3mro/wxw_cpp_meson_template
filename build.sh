237eca76cb994c2e86d3d985adde5ed6c397b6fd
Dr Amr Osman <dr3mro@gmail.com>
2023-05-26 17:52:43 +0300

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
+build_dir=/_build
 if [[ $OSTYPE == 'darwin'* ]]; then
-    [ -d ./_build/ ] && meson setup _build -Ddefault_library=shared --prefix=$(pwd)/_build/application.app --bindir=Contents/MacOS --wipe ||  meson setup _build -Ddefault_library=shared --prefix=$(pwd)/_build/application.app --bindir=Contents/MacOS
-    [ -d ./_build/ ] && cd _build && meson compile && meson install 
+  
+  package=$(pwd)$build_dir/App
+  
+  if [ -d ./_build/ ] 
+    then 
+      meson setup _build -Ddefault_library=static --prefix="$package"/application.app --bindir=Contents/MacOS --wipe 
+    else
+      meson setup _build -Ddefault_library=static --prefix="$package"/application.app --bindir=Contents/MacOS
+  fi  
+  
+  mkdir -p "$package"
+  
+  if [ -d ./_build/ ] &&  cd _build && meson compile && meson install  
+    then
+      if [ "$1" == "run" ] 
+        then
+          open "$package"/application.app 
+      fi
+  fi  
+  
+  ln -s /Applications "$package" 
+
+
+  if [ "$1" != "dmg" ] 
+    then
+     exit 
+  fi
+  
+  if  [ ! -x "$(command -v create-dmg)" ] 
+    then
+      brew install create-dmg && create-dmg application.dmg "$package" 
+    else
+     create-dmg application.dmg "$package" 
+  fi
+
 else
-    [ -d ./_build/ ] && meson setup _build --wipe ||  meson setup _build
+    if [ -d ./_build/ ]
+    then
+      meson setup _build --wipe
+    else
+      meson setup _build
+    fi
+
     [ -d ./_build/ ] && cd _build && meson compile 
 fi
 
